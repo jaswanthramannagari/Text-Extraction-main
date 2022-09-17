b4e40780ddfb8c9c035799e9eaf8d5fd400289f1
suhaas-varma <kvsuhaas@gmail.com>
2022-09-17 15:54:16 +0530

    first

---
+++ Word_separation.py
@@ -0,0 +1,138 @@
+import numpy as np
+import cv2
+
+def sobel(channel):
+    """ The Sobel Operator"""
+    sobelX = cv2.Sobel(channel, cv2.CV_16S, 1, 0)
+    sobelY = cv2.Sobel(channel, cv2.CV_16S, 0, 1)
+    # Combine x, y gradient magnitudes sqrt(x^2 + y^2)
+    sobel = np.hypot(sobelX, sobelY)
+    sobel[sobel > 255] = 255
+    return np.uint8(sobel)
+
+
+def edge_detect(im):
+    """ 
+    Edge detection 
+    The Sobel operator is applied for each image layer (RGB)
+    """
+    return np.max(np.array([sobel(im[:,:, 0]), sobel(im[:,:, 1]), sobel(im[:,:, 2]) ]), axis=0)
+
+def resize(img, height=800, always=False):
+    """Resize image to given height."""
+    if (img.shape[0] > height or always):
+        rat = height / img.shape[0]
+        return cv2.resize(img, (int(rat * img.shape[1]), height))
+    
+    return img
+
+
+def ratio(img, height=800):
+    """Getting scale ratio."""
+    return img.shape[0] / height
+
+def union(a,b):
+    x = min(a[0], b[0])
+    y = min(a[1], b[1])
+    w = max(a[0]+a[2], b[0]+b[2]) - x
+    h = max(a[1]+a[3], b[1]+b[3]) - y
+    return [x, y, w, h]
+
+def intersect(a,b):
+    x = max(a[0], b[0])
+    y = max(a[1], b[1])
+    w = min(a[0]+a[2], b[0]+b[2]) - x
+    h = min(a[1]+a[3], b[1]+b[3]) - y
+    if w<0 or h<0:
+        return False
+    return True
+
+def group_rectangles(rec):
+    """
+    Uion intersecting rectangles
+    Args:
+        rec - list of rectangles in form [x, y, w, h]
+    Return:
+        list of grouped ractangles 
+    """
+    tested = [False for i in range(len(rec))]
+    final = []
+    i = 0
+    while i < len(rec):
+        if not tested[i]:
+            j = i+1
+            while j < len(rec):
+                if not tested[j] and intersect(rec[i], rec[j]):
+                    rec[i] = union(rec[i], rec[j])
+                    tested[j] = True
+                    j = i
+                j += 1
+            final += [rec[i]]
+        i += 1
+            
+    return final
+
+def text_detect(img, original):
+    """ Text detection using contours """
+    # Resize image
+    small = resize(img, 2000)
+    image = resize(original, 2000)
+    
+    # Finding contours
+    mask = np.zeros(small.shape, np.uint8)
+    cnt, hierarchy = cv2.findContours(np.copy(small), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Variables for contour index and words' bounding boxes
+    index = 0    
+    boxes = []
+    # CCOMP hierarchy: [Next, Previous, First Child, Parent]
+    # cv2.RETR_CCOMP - contours into 2 levels
+    # Go through all contours in first level
+    while (index >= 0):
+        x,y,w,h = cv2.boundingRect(cnt[index])
+        # Get only the contour
+        cv2.drawContours(mask, cnt, index, (255, 255, 255), cv2.FILLED)
+        maskROI = mask[y:y+h, x:x+w]
+        # Ratio of white pixels to area of bounding rectangle
+        r = cv2.countNonZero(maskROI) / (w * h)
+        
+        # Limits for text (white pixel ratio, width, height)
+        # TODO Test h/w and w/h ratios
+        if r > 0.1 and 2000 > w > 10 and 1600 > h > 10 and h/w < 3 and w/h < 10:
+            boxes += [[x, y, w, h]]
+        
+        # Index of next contour
+        index = hierarchy[0][index][0]
+        
+    # Group intersecting rectangles
+    boxes = group_rectangles(boxes)
+    bounding_boxes = np.array([0,0,0,0])
+    for (x, y, w, h) in boxes:
+        cv2.rectangle(image, (x, y),(x+w,y+h), (0, 255, 0), 8)
+        bounding_boxes = np.vstack((bounding_boxes, np.array([x, y, x+w, y+h])))
+
+    # Recalculate coordinates to original scale
+    boxes = bounding_boxes.dot(ratio(image, small.shape[0])).astype(np.int64)
+    return boxes[1:]  
+
+def sort_words(boxes):
+    """Sort boxes - (x, y, x+w, y+h) from left to right, top to bottom."""
+    mean_height = sum([y2 - y1 for _, y1, _, y2 in boxes]) / len(boxes)
+    
+    boxes.view('i8,i8,i8,i8').sort(order=['f1'], axis=0)
+    current_line = boxes[0][1]
+    lines = []
+    tmp_line = []
+    for box in boxes:
+        if box[1] > current_line + mean_height:
+            lines.append(tmp_line)
+            tmp_line = [box]
+            current_line = box[1]            
+            continue
+        tmp_line.append(box)
+    lines.append(tmp_line)
+        
+    for line in lines:
+        line.sort(key=lambda box: box[0])
+        
+    return lines
